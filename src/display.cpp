aa056a40d744c6e3f401f998405f14af9558185e
Tony <x0sin0x@gmail.com>
2025-06-27 20:55:02 -0600

    [BOOT] Setup() started [DISPLAY] TFT initialized and splash loaded [WIFI] Access Point started

---
+++ src/display.cpp
@@ -0,0 +1,2 @@
+#include "LGFX_Config.hpp"
+LGFX tft;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
-// Entry point
-#include "config.h"
+#include <Arduino.h>
+#include <WiFi.h>
+#include "webui/web_server.hpp"
+#include "display/LGFX_Config.hpp"
+#include "display/image_loader.hpp"
+#include "webui/api_log.hpp"
+
+LGFX tft;
+
 void setup() {
-  setupConfig();
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("[BOOT] Setup() started");
+  logln("[BOOT] Setup() started");
+
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  draw_image(tft, "/n3wb_main.png");
+  logln("[DISPLAY] TFT initialized and splash loaded");
+
+  WiFi.softAP("n3wbDuck", "newbpass");
+  Serial.println("[WIFI] Access Point started: n3wbDuck");
+  logln("[WIFI] Access Point started");
+
+  start_web_server();
+  logln("[WEB] Web server started");
 }
-void loop() {}
+
+void loop() {
+  delay(100);
+}
